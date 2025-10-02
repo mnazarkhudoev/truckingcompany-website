f7afa0afd2ec809e4f678dc675aca3e5823a8e3e
mnazarkhudoev <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2025-10-02 19:47:55 +0600

    basename adjusted for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+// Get base path for GitHub Pages deployment
+const basename = import.meta.env.PROD ? '/truckingcompany-website' : '';
+
 createRoot(document.getElementById('root')).render(<StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <App />
         </BrowserRouter>
     </StrictMode>);
