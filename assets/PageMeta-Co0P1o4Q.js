44597d55bef25dfacb17399b97067ee358cfddec
mnazarkhudoev <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2025-10-02 19:44:38 +0600

    Updates

---
+++ assets/PageMeta-Co0P1o4Q.js
@@ -0,0 +1,1 @@
+import{j as e,H as o}from"./index-C0131-lf.js";const a=({title:r,description:t,icon:n})=>e.jsxs(e.Fragment,{children:[e.jsx("title",{children:r??"Iron Bull Trucking"}),t&&e.jsx("meta",{property:"og:description",content:t}),e.jsx("link",{rel:"icon",href:n??o,type:"image/png"})]});export{a as P};
