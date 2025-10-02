8678fe5dc6e9c23a2a2da94f6a474ea8aade6cb6
mnazarkhudoev <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2025-10-02 19:30:33 +0600

    getting ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/truckingcompany-website/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
